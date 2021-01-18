ac5f184fcd146711184b422e4ccb7d84205e767b
Luiz Gustavo <luiz.gustavo@serel.com.br>
2021-01-18 11:45:14 -0300

    Arquivos principais

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <div style="text-align:center">
+            <tittle>Meu síte</tittle>
+            <link rel="shortcut icon" href="../imagens/icone.png">
+            
+            <link rel="stylesheet" type="text/css" href="../css/index.css">
+        </div>
+    </head>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <div style="text-align:center">
+            <tittle>Meu síte</tittle>
+            <link rel="shortcut icon" href="../imagens/icone.png">
+            
+            <link rel="stylesheet" type="text/css" href="../css/index.css">
+        </div>
+    </head>
+
+    <body>
+        <a href="../ajax/galeria.php" class="button-gallery btn1">Galeria</a>
+        <a href="../ajax/contato.php" class="button-contact btn2">Contato</a>
+    </body>
+</html>
