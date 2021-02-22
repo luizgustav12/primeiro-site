8245a89f3bb147103a0892863847d65352528c04
Luiz Gustavo <luiz.gustavo@serel.com.br>
2021-02-22 13:11:37 -0300

    Arquivos dia 22/02

--- system/galeria.php
+++ system/galeria.php
@@ -9,15 +9,29 @@
     <body>
         
         <div class="class-body">
-            <div class="img">
-                <a class="fotos">
-                    <img src="../imagens/fotos/foto1.jpg">
-                </a>
-
-                <a class="fotos">
-                    <img src="../imagens/fotos/foto2.jpg">
-                </a>
+            <div class="fotos">
+                <div class="class-imagem"> <img src="../imagens/fotos/foto1.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto2.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto3.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto4.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto5.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto6.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto7.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto8.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto9.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto10.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto11.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto12.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto13.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto14.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto15.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto16.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto17.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto18.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto19.jpg"> </div>
+                <div class="class-imagem"> <img src="../imagens/fotos/foto20.jpg"> </div>
             </div>
+          
         </div>
     </body>
 
